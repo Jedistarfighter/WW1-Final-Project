af79c77d3f6b547b609199876c3854937e4b462b
Jedistarfighter <133034412+Jedistarfighter@users.noreply.github.com>
2023-05-19 09:19:17 -0600

    Update main.cpp
    Added Main Menu Start

--- main.cpp
+++ main.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <sstream>
 #include <fstream>
-#include <string>
-#include <unordered_map>
-#include <vector>
+#include "CharacterSelect.h"
 using namespace std;
+enum Choices {NEW = 1, QUIT};
+
+void characterSelection();
+int mainMenu();
 
 int main() {
+  // Variable Setup
+  int input;
+
+  // Struct Setup
+    struct characterStats{
+      string name;
+      string country;
+      string division;
+      int age;
+      int strength;
+      int health;
+    };
+
+  // Character Selection
+  do {
+    input = mainMenu();
+    switch(input) {
+      case NEW: 
+        system("clear");
+        characterSelection();
+      default:
+        system("clear");
+        break;
+    }
+  } while(input != QUIT);
+  
+}
+
+// Main Menu
+int mainMenu(){
   
 }
