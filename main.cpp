7a77f317015ce99f632197299ce75880e0df1229
Jedistarfighter <133034412+Jedistarfighter@users.noreply.github.com>
2023-05-19 10:06:34 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 enum Choices {NEW = 1, QUIT};
 
 void characterSelection();
+void characterSearch();
+void characterCreate();
 int mainMenu();
 
 int main() {
@@ -41,5 +43,37 @@ int main() {
 
 // Main Menu
 int mainMenu(){
-  
+  int input; 
+  cout << "Welcome To My WW1 Text Based Adventure Game!\n" << endl;
+  cout << "1. New Game" << endl;
+  cout << "2. Quit" << endl;
+  cin >> input;
+  return input;
 }
+
+void characterSelection(){
+  int input; 
+  cout << "What would you like to do?\n" << endl;
+  cout << "1. Search for Character" << endl;
+  cout << "2. Create new Character" << endl;
+  cin >> input;
+  do {
+    switch(input) {
+    case 1: 
+      system("clear");
+      characterSearch();
+    case 2: 
+      system("clear");
+      characterCreate();
+    default:
+      system("clear");
+      break;
+    }
+  } while (input > 2);
+}
+
+// Character Select Function
+void characterSearch(){}
+
+// Character Creation Function
+void characterCreate(){}
